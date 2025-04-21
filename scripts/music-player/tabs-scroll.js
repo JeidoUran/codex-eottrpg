2fda540c8b7085be917d83c4a76494e9dfae493e
Jeido <tourais.q@gmail.com>
2025-04-21 16:00:33 +0200

    rework music page, new buttons, fix bugs, put scripts in their own files

---
+++ scripts/music-player/tabs-scroll.js
@@ -0,0 +1,26 @@
+const scrollBtns = document.querySelectorAll('.scroll-btn');
+const scrollContainer = document.querySelector('.playlist-tabs-container');
+scrollBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const scrollAmount = 150;
+    if (btn.classList.contains('left')) {
+      scrollContainer.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth'
+      });
+    } else {
+      scrollContainer.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const defaultTab = document.querySelector('.tab-button[data-playlist="sys"]');
+    if (defaultTab) defaultTab.click();
+    // Forcer le scroll au tout début
+    const tabsContainer = document.querySelector('.playlist-tabs-container');
+    tabsContainer.scrollLeft = 0;
+});
