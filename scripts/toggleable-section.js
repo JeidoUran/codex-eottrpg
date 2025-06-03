8d7c1b394c4b208060c95aef7b66de8d333289ba
Jeido <tourais.q@gmail.com>
2025-06-03 22:25:55 +0200

    prettier

--- scripts/toggleable-section.js
+++ scripts/toggleable-section.js
@@ -1,5 +1,7 @@
 function toggleSection(button) {
-  const content = button.closest("section").querySelector(".collapsible-content");
+  const content = button
+    .closest("section")
+    .querySelector(".collapsible-content");
   const isOpen = content.classList.contains("expanded");
   if (isOpen) {
     content.classList.remove("expanded");
